a55fda11343d0a801b0409184c7e5e5519839570
pimpahoe <fasthunt1337@gmail.com>
2026-07-01 20:24:41 +0300

    проверка времени

--- celery_app.py
+++ celery_app.py
@@ -16,7 +16,7 @@ app = Celery(
 app.conf.beat_schedule = {
     "parse-yandex-every-5-minutes": {
         "task": "parser.tasks.parse_flats_task",
-        "schedule": 600.0,  \
+        "schedule": 60.0,  \
     },
 }
 app.conf.timezone = "UTC"
