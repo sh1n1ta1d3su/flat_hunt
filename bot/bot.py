fcb0824f599ab1eba694121db730d552153a8525
pimpahoe <fasthunt1337@gmail.com>
2026-07-01 20:34:04 +0300

    proxy

--- bot/bot.py
+++ bot/bot.py
@@ -1,19 +1,33 @@
 import asyncio
 import os
 import sys
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.client.session.aiohttp import AiohttpSession
+
+from aiohttp import ClientSession
+from aiohttp_socks import ProxyConnector
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from database import set_search_url, get_search_url, init_db
 
+
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-USER_ID = int(os.getenv("TELEGRAM_USER_ID","0"))
+USER_ID = int(os.getenv("TELEGRAM_USER_ID", "0"))
+
+proxy_url = "socks5://login:password@ip:port"
+
+connector = ProxyConnector.from_url(proxy_url)
+aiohttp_session = ClientSession(connector=connector)
+
+session = AiohttpSession(client_session=aiohttp_session)
+
+bot = Bot(token=BOT_TOKEN, session=session)
+dp = Dispatcher()
 
 
 async def send(area, metro, distance, clean_price, full_url, img_url):
-    bot = Bot(token=BOT_TOKEN)
-
     text = (
         f"🏠 <b>Новая квартира!</b>\n"
         f"💰 Цена: {clean_price}\n"
@@ -22,26 +36,19 @@ async def send(area, metro, distance, clean_price, full_url, img_url):
         f"🔗 <a href='{full_url}'>Перейти к объявлению</a>"
     )
 
-    try:
-        # Если ссылка на картинку нормальная, отправляем фото с подписью
-        if img_url.startswith("http"):
-            await bot.send_photo(
-                chat_id=USER_ID,
-                photo=img_url,
-                caption=text,
-                parse_mode="HTML"
-            )
-        else:
-            # Если фото нет, отправляем просто текст
-            await bot.send_message(
-                chat_id=USER_ID,
-                text=text,
-                parse_mode="HTML"
-            )
-    finally:
-        # Закрываем сессию, чтобы не было таймаутов
-        await bot.session.close()
-
+    if img_url.startswith("http"):
+        await bot.send_photo(
+            chat_id=USER_ID,
+            photo=img_url,
+            caption=text,
+            parse_mode="HTML"
+        )
+    else:
+        await bot.send_message(
+            chat_id=USER_ID,
+            text=text,
+            parse_mode="HTML"
+        )
 dp = Dispatcher()
 
 
@@ -82,7 +89,6 @@ async def cmd_current(message: types.Message):
 
 async def main():
     init_db()
-    bot = Bot(token=BOT_TOKEN)
     print("Бот запущен!")
     await dp.start_polling(bot)
 
